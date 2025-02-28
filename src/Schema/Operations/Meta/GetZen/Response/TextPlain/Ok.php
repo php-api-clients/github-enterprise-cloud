3c8e268b850f0d10c6a1aa3e009962a07a7b913e
WyriHaximus <147145+WyriHaximus@users.noreply.github.com>
2025-02-28 20:59:38 +0000

    [GitHubEnterpriseCloud] Update to 1.1.4-aa6e867dcdb64b9db1fac624925d5631 from 1.1.4-1af67ef4f45d37f00bce6c1060039205 Detected Schema changes: 2025-02-28 20:29:03 ERROR unable to open the rolodex file, check specification references and base path                       ├ file: /__w/github-root/github-root/server-statistics-actions.yaml                       └ error: open /__w/github-root/github-root/server-statistics-actions.yaml: no such file or directory 2025-02-28 20:29:03 ERROR unable to open the rolodex file, check specification references and base path                       ├ file: /__w/github-root/github-root/server-statistics-packages.yaml                       └ error: open /__w/github-root/github-root/server-statistics-packages.yaml: no such file or directory 2025-02-28 20:29:03 ERROR unable to open the rolodex file, check specification references and base path                       ├ file: /__w/github-root/github-root/server-statistics-advisory-db.yaml                       └ error: open /__w/github-root/github-root/server-statistics-advisory-db.yaml: no such file or directory 2025-02-28 20:29:04 ERROR unable to open the rolodex file, check specification references and base path                       ├ file: /__w/github-root/github-root/server-statistics-packages.yaml                       └ error: open /__w/github-root/github-root/server-statistics-packages.yaml: no such file or directory 2025-02-28 20:29:04 ERROR unable to open the rolodex file, check specification references and base path                       ├ file: /__w/github-root/github-root/server-statistics-advisory-db.yaml                       └ error: open /__w/github-root/github-root/server-statistics-advisory-db.yaml: no such file or directory 2025-02-28 20:29:04 ERROR unable to open the rolodex file, check specification references and base path                       ├ file: /__w/github-root/github-root/server-statistics-actions.yaml                       └ error: open /__w/github-root/github-root/server-statistics-actions.yaml: no such file or directory ERROR: component `server-statistics-actions.yaml` does not exist in the specification ERROR: component `server-statistics-packages.yaml` does not exist in the specification ERROR: component `server-statistics-advisory-db.yaml` does not exist in the specification ERROR: cannot resolve reference `server-statistics-actions.yaml`, it's missing: $.['server-statistics-actions.yaml'] [215922:11] ERROR: cannot resolve reference `server-statistics-packages.yaml`, it's missing: $.['server-statistics-packages.yaml'] [215924:11] ERROR: cannot resolve reference `server-statistics-advisory-db.yaml`, it's missing: $.['server-statistics-advisory-db.yaml'] [215926:11] ERROR: component `server-statistics-packages.yaml` does not exist in the specification ERROR: component `server-statistics-advisory-db.yaml` does not exist in the specification ERROR: component `server-statistics-actions.yaml` does not exist in the specification ERROR: cannot resolve reference `server-statistics-actions.yaml`, it's missing: $.['server-statistics-actions.yaml'] [215960:11] ERROR: cannot resolve reference `server-statistics-packages.yaml`, it's missing: $.['server-statistics-packages.yaml'] [215962:11] ERROR: cannot resolve reference `server-statistics-advisory-db.yaml`, it's missing: $.['server-statistics-advisory-db.yaml'] [215964:11]

--- src/Schema/Operations/Meta/GetZen/Response/TextPlain/Ok.php
+++ src/Schema/Operations/Meta/GetZen/Response/TextPlain/Ok.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace ApiClients\Client\GitHubEnterpriseCloud\Schema\Operations\Meta\GetZen\Response\ApplicationJson;
+namespace ApiClients\Client\GitHubEnterpriseCloud\Schema\Operations\Meta\GetZen\Response\TextPlain;
 
 use ApiClients\Client\GitHubEnterpriseCloud\Schema;
 
